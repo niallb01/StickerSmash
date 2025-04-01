c161cabac548543f23938355913a0ed69fd2e2a4
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2025-04-01 14:51:52 +0100

    first

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Yo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: { color: "#fff" },
+});
