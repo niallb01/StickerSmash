6444e21ec728f752178203eb9a58c26b4801935f
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2025-04-02 19:17:01 +0100

    not found

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,14 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Home", headerShown: true }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{ title: "About", headerShown: true }}
+      />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Yowza</Text>
+      <Text style={styles.text}>Home</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen
+      </Link>
     </View>
   );
 }
@@ -16,4 +20,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: { color: "#fff" },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
 });
