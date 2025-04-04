17e50859081ced0f1a56fa5137a11ee9b4dc2a82
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2025-04-04 11:23:30 +0100

    add image

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,10 +3,14 @@ import { Image, type ImageSource } from "expo-image";
 
 type Props = {
   imgSource: ImageSource;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props) {
-  return <Image source={imgSource} style={styles.image} />;
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  // If the selected image is not null, show the image from the device, otherwise, show the placeholder image.
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
